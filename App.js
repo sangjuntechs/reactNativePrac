8e594f8ccc578b666175548dc9e63febb0ac6339
sangjunPark <juntech0421@github.com>
2020-05-16 02:43:00 +0900

    check react-native

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>안녕하세요. 박상준의 앱입니다.</Text>
+      <Text>ReactNative를 이용한 IOS APP을 빌드합니다.</Text>
     </View>
   );
 }
@@ -12,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # reactNativePrac
 React Native로 날씨 앱을 빌드하는 프로젝트.
+
+expo로 휴대폰과 연결 성공, react-native의 작동방식 이해.
