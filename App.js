ef4f69451929bcb9b7049c9602d9621322686cd4
sangjunPark <juntech0421@github.com>
2020-05-17 20:00:22 +0900

    add code

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import { Alert } from 'react-native';
 
-export default function App() {
-  return <Loading/>;
+export default class extends React.Component{
+  state = {
+    isLoading :true
+  };
+  getLoaction = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords : { latitude, longitude}
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading : false});
+    } catch (error) {
+      Alert.alert("위치를 찾을 수 없습니다", "옵션에서 위치 찾기를 활성화 시키세요.");
+    }
+    
+  };
+  componentDidMount() {
+    this.getLoaction();
+  }
+  render() {
+    const { isLoading} = this.state;
+    return isLoading ? <Loading/> : null;
+  }
 }
 
 
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ expo로 휴대폰과 연결 성공, react-native의 작동방식 이해.
 
 ## `2020/05/17`
 React flexbox에 대한 이해.
+Loading.js 추가 import방식과 컴포넌트 사용은 기존 리액트와 
+비슷한 방식 expo-location api를 import후 사용.
+alert 사용 및 expo location 의 사용 및 loading.js 코드 추가
